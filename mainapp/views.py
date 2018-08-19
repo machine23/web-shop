d352f8e27ef1b74920fada3c9ef05b45dfa8aaac
Vassili Baranov <vassili.baranov@gmail.com>
2018-08-19 19:31:53 +0300

    rebuild templates

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,15 +2,57 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    featured = []
+    trending = []
+    for i in range(1, 5):
+        product = {
+            'image': 'img/product-{}.jpg'.format(i),
+            'title': 'Product {}'.format(i),
+        }
+        featured.append(product)
+
+    for i in range(1, 7):
+        product = {
+            'image': 'img/product-{}1.jpg'.format(i),
+            'title': 'Product {}'.format(i),
+        }
+        trending.append(product)
+
+    context = {
+        'featured': featured,
+        'trending': trending,
+    }
+    return render(request, 'mainapp/index.html', context)
 
 
 def details(request):
-    return render(request, 'mainapp/details.html')
+    products = []
+    for i in range(1, 4):
+        product = {
+            'image': 'img/product-{}1.jpg'.format(i),
+            'title': 'Product {}'.format(i),
+        }
+        products.append(product)
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'mainapp/details.html', context)
     
     
 def products(request):
-    return render(request, 'mainapp/products.html')
+    products = []
+    for i in range(1, 7):
+        product = {
+            'image': 'img/product-{}1.jpg'.format(i),
+            'title': 'Product {}'.format(i),
+        }
+        products.append(product)
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'mainapp/products.html', context)
     
     
 def contacts(request):
